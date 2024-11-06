10fbb81783d0c3774aeebbf5a7787de14adf9abd
adirvaknin <adirvaknin2014@gmail.com>
2024-11-07 00:23:48 +0200

    second one

--- src/JavaProject.java
+++ src/JavaProject.java
@@ -3,5 +3,8 @@ public class JavaProject {
         int y = 2024;
         System.out.println(y);
         // I wanna see this message in the description as well.
+        System.out.print("Hello World!, How you doing? Donald J Trump won today and elected did you see?!?");
+        double j = 14.14;
+        System.out.println(j);
     }
 }
