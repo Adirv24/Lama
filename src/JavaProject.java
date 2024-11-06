1777f7ae79c72c2ae0a6015cbdac0f1380c023b3
adirvaknin <adirvaknin2014@gmail.com>
2024-11-07 00:20:01 +0200

    first one

---
+++ src/JavaProject.java
@@ -0,0 +1,7 @@
+public class JavaProject {
+    public static void main(String[] args) {
+        int y = 2024;
+        System.out.println(y);
+        // I wanna see this message in the description as well.
+    }
+}
